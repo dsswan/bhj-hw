edcd410b15c0bf1aea304c5435e192a0ed1b0045
dsswan <ds.swan@mail.ru>
2022-07-11 13:23:16 +0500

    hw-01. js-featutes

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,9 @@
+let counter = document.getElementById("clicker__counter"),
+  cookie = document.getElementById("cookie"),
+  score = parseInt(counter.textContent);
+
+cookie.onclick = function () {
+  score += 1;
+  counter.innerHTML = score;
+  cookie.style.width = score % 2 ? "250px" : "200px";
+};
---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+let timer = document.getElementById("timer");
+let currentTime = parseInt(timer.textContent);
+
+let timerId = setTimeout(function tick() {
+  currentTime -= 1;
+  if (currentTime !== 0) {
+    timer.innerHTML = currentTime;
+  } else {
+    alert("Вы победили в конкурсе!");
+    clearTimeout(timerId);
+    return;
+  }
+  timerId = setTimeout(tick, 1000);
+}, 1000);
